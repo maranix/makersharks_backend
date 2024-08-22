48f42b1d2c8af64dc3a017f861d83b3f320d4092
maranix <ramanverma4183@gmail.com>
2024-08-23 00:55:02 +0530

    add support for documentation using Swagger-UI and OpenAPI v3

--- src/main/java/com/example/makersharks/controller/PingController.java
+++ src/main/java/com/example/makersharks/controller/PingController.java
@@ -7,12 +7,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.makersharks.model.Response;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
 @RestController
 @RequestMapping("/api")
 public class PingController {
 
     private static final String pong = "Pong!";
 
+    @Operation(description = "Ping the server")
+    @ApiResponse(responseCode = "200", description = "Successful ping response with status and message")
     @GetMapping("/ping")
     public Response<String> getPing() {
         return Response.success(HttpStatus.OK, pong);
